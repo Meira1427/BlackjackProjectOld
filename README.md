38968630f748314ffa524600ba43e0f0da4bff5f
Meira Pentermann <meira@meirapentermann.com>
2017-08-18 16:05:58 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Blackjack Project
+
+### Meira Pentermann 08-18-2017
+
+### Overview
+This project is a command-line blackjack game
+
+### Topics Covered
+Inheritance
+Object-Oriented Programming
+Link to Rules - Add
---
+++ src/com/meirapentermann/cards/Suit.java
@@ -0,0 +1,17 @@
+package com.meirapentermann.cards;
+
+public enum Suit {
+	  HEARTS("Hearts", 	'\u2665') 		//try also 2661
+	, SPADES("Spades", 	'\u2660') 		//try also 2664
+	, CLUBS("Clubs", 	'\u2663') 		//try also 2667
+	, DIAMONDS("Diamonds", '\u2666'); 	//try also 2662
+	
+	public String displayText;
+	public char displayChar;
+	
+	private Suit(String s, char c) {
+		this.displayText = s;
+		this.displayChar = c;
+	}
+
+}
